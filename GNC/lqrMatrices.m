6cfffa6dfd5260c8b30c6cb72c18a05f8f66bc86
José Reynaldo <ja.reynaldo@outlook.com>
2025-02-20 14:29:52 -0500

    Revert "Merge branch 'main' of https://github.com/florida-rocket-lab/Complete-Skipper-Software"
    This reverts commit 13cc1ff0b8769142ffc36a94db7c715b61c009b3, reversing
    changes made to bffd2be8101c2094ce45ee122ae6d50453109086.

--- GNC/lqrMatrices.m
+++ GNC/lqrMatrices.m
@@ -23,18 +23,18 @@ function [A, B, C, D, K, d] = lqrMatrices(x0, y0, z0, theta0, u0, v0, w0, psi0, 
 
     % BEGIN "A"; Do not modify this. The python script looks for this line.
     A = [
-        cos(psi0)*cos(theta0) sin(phi0)*sin(theta0)*cos(psi0)-sin(psi0)*cos(phi0) sin(phi0)*sin(psi0)+sin(theta0)*cos(phi0)*cos(psi0) v0*sin(phi0)*sin(psi0)+v0*sin(theta0)*cos(phi0)*cos(psi0)-w0*sin(phi0)*sin(theta0)*cos(psi0)+w0*sin(psi0)*cos(phi0) -u0*sin(theta0)*cos(psi0)+v0*sin(phi0)*cos(psi0)*cos(theta0)+w0*cos(phi0)*cos(psi0)*cos(theta0) -u0*sin(psi0)*cos(theta0)-v0*sin(phi0)*sin(psi0)*sin(theta0)-v0*cos(phi0)*cos(psi0)+w0*sin(phi0)*cos(psi0)-w0*sin(psi0)*sin(theta0)*cos(phi0) 0 0 0;
-        sin(psi0)*cos(theta0) sin(phi0)*sin(psi0)*sin(theta0)+cos(phi0)*cos(psi0) -sin(phi0)*cos(psi0)+sin(psi0)*sin(theta0)*cos(phi0) -v0*sin(phi0)*cos(psi0)+v0*sin(psi0)*sin(theta0)*cos(phi0)-w0*sin(phi0)*sin(psi0)*sin(theta0)-w0*cos(phi0)*cos(psi0) -u0*sin(psi0)*sin(theta0)+v0*sin(phi0)*sin(psi0)*cos(theta0)+w0*sin(psi0)*cos(phi0)*cos(theta0) u0*cos(psi0)*cos(theta0)+v0*sin(phi0)*sin(theta0)*cos(psi0)-v0*sin(psi0)*cos(phi0)+w0*sin(phi0)*sin(psi0)+w0*sin(theta0)*cos(phi0)*cos(psi0) 0 0 0;
-        -sin(theta0) sin(phi0)*cos(theta0) cos(phi0)*cos(theta0) v0*cos(phi0)*cos(theta0)-w0*sin(phi0)*cos(theta0) -u0*cos(theta0)-v0*sin(phi0)*sin(theta0)-w0*sin(theta0)*cos(phi0) 0 0 0 0;
-        0 r0 -q0 0 g*sin(theta0)*cos(psi0) g*sin(psi0)*cos(theta0) 0 -w0 v0;
-        -r0 0 p0 -g*sin(phi0)*sin(psi0)-g*sin(theta0)*cos(phi0)*cos(psi0) -g*sin(phi0)*cos(psi0)*cos(theta0) g*sin(phi0)*sin(psi0)*sin(theta0)+g*cos(phi0)*cos(psi0) w0 0 -u0;
-        q0 -p0 0 g*sin(phi0)*sin(theta0)*cos(psi0)-g*sin(psi0)*cos(phi0) -g*cos(phi0)*cos(psi0)*cos(theta0) -g*sin(phi0)*cos(psi0)+g*sin(psi0)*sin(theta0)*cos(phi0) -v0 u0 0;
-        0 0 0 q0*cos(phi0)*tan(theta0)-r0*sin(phi0)*tan(theta0) q0*sin(phi0)*tan(theta0)^2+q0*sin(phi0)+r0*cos(phi0)*tan(theta0)^2+r0*cos(phi0) 0 1 sin(phi0)*tan(theta0) cos(phi0)*tan(theta0);
-        0 0 0 -q0*sin(phi0)-r0*cos(phi0) 0 0 0 cos(phi0) -sin(phi0);
-        0 0 0 q0*cos(phi0)/cos(theta0)-r0*sin(phi0)/cos(theta0) q0*sin(phi0)*sin(theta0)/cos(theta0)^2+r0*sin(theta0)*cos(phi0)/cos(theta0)^2 0 0 sin(phi0)/cos(theta0) cos(phi0)/cos(theta0);
-        0 0 0 0 0 0 0 Iyy*r0/Ixx-Izz*r0/Ixx Iyy*q0/Ixx-Izz*q0/Ixx;
-        0 0 0 0 0 0 -Ixx*r0/Iyy+Izz*r0/Iyy 0 -Ixx*p0/Iyy+Izz*p0/Iyy;
-        0 0 0 0 0 0 Ixx*q0/Izz-Iyy*q0/Izz Ixx*p0/Izz-Iyy*p0/Izz 0;
+        0 r0 -q0 cos(psi0)*cos(theta0) sin(phi0)*sin(theta0)*cos(psi0)-sin(psi0)*cos(phi0) sin(phi0)*sin(psi0)+sin(theta0)*cos(phi0)*cos(psi0) v0*sin(phi0)*sin(psi0)+v0*sin(theta0)*cos(phi0)*cos(psi0)-w0*sin(phi0)*sin(theta0)*cos(psi0)+w0*sin(psi0)*cos(phi0) -u0*sin(theta0)*cos(psi0)+v0*sin(phi0)*cos(psi0)*cos(theta0)+w0*cos(phi0)*cos(psi0)*cos(theta0) -u0*sin(psi0)*cos(theta0)-v0*sin(phi0)*sin(psi0)*sin(theta0)-v0*cos(phi0)*cos(psi0)+w0*sin(phi0)*cos(psi0)-w0*sin(psi0)*sin(theta0)*cos(phi0) 0 -z0 y0;
+        -r0 0 p0 sin(psi0)*cos(theta0) sin(phi0)*sin(psi0)*sin(theta0)+cos(phi0)*cos(psi0) -sin(phi0)*cos(psi0)+sin(psi0)*sin(theta0)*cos(phi0) -v0*sin(phi0)*cos(psi0)+v0*sin(psi0)*sin(theta0)*cos(phi0)-w0*sin(phi0)*sin(psi0)*sin(theta0)-w0*cos(phi0)*cos(psi0) -u0*sin(psi0)*sin(theta0)+v0*sin(phi0)*sin(psi0)*cos(theta0)+w0*sin(psi0)*cos(phi0)*cos(theta0) u0*cos(psi0)*cos(theta0)+v0*sin(phi0)*sin(theta0)*cos(psi0)-v0*sin(psi0)*cos(phi0)+w0*sin(phi0)*sin(psi0)+w0*sin(theta0)*cos(phi0)*cos(psi0) z0 0 -x0;
+        q0 -p0 0 -sin(theta0) sin(phi0)*cos(theta0) cos(phi0)*cos(theta0) v0*cos(phi0)*cos(theta0)-w0*sin(phi0)*cos(theta0) -u0*cos(theta0)-v0*sin(phi0)*sin(theta0)-w0*sin(theta0)*cos(phi0) 0 -y0 x0 0;
+        q0^2+r0^2 -p0*q0 -p0*r0 0 r0 -q0 0 g*sin(theta0)*cos(psi0) g*sin(psi0)*cos(theta0) -q0*y0-r0*z0 -p0*y0+2*q0*x0-w0 -p0*z0+2*r0*x0+v0;
+        -p0*q0 p0^2+r0^2 -q0*r0 -r0 0 p0 -g*sin(phi0)*sin(psi0)-g*sin(theta0)*cos(phi0)*cos(psi0) -g*sin(phi0)*cos(psi0)*cos(theta0) g*sin(phi0)*sin(psi0)*sin(theta0)+g*cos(phi0)*cos(psi0) 2*p0*y0-q0*x0+w0 -p0*x0-r0*z0 -q0*z0+2*r0*y0-u0;
+        -p0*r0 -q0*r0 p0^2+q0^2 q0 -p0 0 g*sin(phi0)*sin(theta0)*cos(psi0)-g*sin(psi0)*cos(phi0) -g*cos(phi0)*cos(psi0)*cos(theta0) -g*sin(phi0)*cos(psi0)+g*sin(psi0)*sin(theta0)*cos(phi0) 2*p0*z0-r0*x0-v0 2*q0*z0-r0*y0+u0 -p0*x0-q0*y0;
+        0 0 0 0 0 0 q0*sin(theta0)*cos(phi0)/cos(theta0)-r0*sin(phi0)*sin(theta0)/cos(theta0) q0*sin(phi0)/cos(theta0)^2+r0*cos(phi0)/cos(theta0)^2 0 1 sin(phi0)*sin(theta0)/cos(theta0) sin(theta0)*cos(phi0)/cos(theta0);
+        0 0 0 0 0 0 -q0*sin(phi0)-r0*cos(phi0) 0 0 0 cos(phi0) -sin(phi0);
+        0 0 0 0 0 0 q0*cos(phi0)/cos(theta0)-r0*sin(phi0)/cos(theta0) q0*sin(phi0)*sin(theta0)/cos(theta0)^2+r0*sin(theta0)*cos(phi0)/cos(theta0)^2 0 0 sin(phi0)/cos(theta0) cos(phi0)/cos(theta0);
+        0 0 0 0 0 0 0 0 0 0 Iyy*r0/Ixx-Izz*r0/Ixx Iyy*q0/Ixx-Izz*q0/Ixx;
+        0 0 0 0 0 0 0 0 0 -Ixx*r0/Iyy+Izz*r0/Iyy 0 -Ixx*p0/Iyy+Izz*p0/Iyy;
+        0 0 0 0 0 0 0 0 0 Ixx*q0/Izz-Iyy*q0/Izz Ixx*p0/Izz-Iyy*p0/Izz 0;
     ];
     % END "A"; Do not modify this. The python script looks for this line.
 
@@ -59,13 +59,13 @@ function [A, B, C, D, K, d] = lqrMatrices(x0, y0, z0, theta0, u0, v0, w0, psi0, 
 
     % BEGIN "d"; Do not modify this. The python script looks for this line.
     d = [
-        -phi0*v0*sin(phi0)*sin(psi0) - phi0*v0*sin(theta0)*cos(phi0)*cos(psi0) + phi0*w0*sin(phi0)*sin(theta0)*cos(psi0) - phi0*w0*sin(psi0)*cos(phi0) + psi0*u0*sin(psi0)*cos(theta0) + psi0*v0*sin(phi0)*sin(psi0)*sin(theta0) + psi0*v0*cos(phi0)*cos(psi0) - psi0*w0*sin(phi0)*cos(psi0) + psi0*w0*sin(psi0)*sin(theta0)*cos(phi0) + theta0*u0*sin(theta0)*cos(psi0) - theta0*v0*sin(phi0)*cos(psi0)*cos(theta0) - theta0*w0*cos(phi0)*cos(psi0)*cos(theta0);
-        phi0*v0*sin(phi0)*cos(psi0) - phi0*v0*sin(psi0)*sin(theta0)*cos(phi0) + phi0*w0*sin(phi0)*sin(psi0)*sin(theta0) + phi0*w0*cos(phi0)*cos(psi0) - psi0*u0*cos(psi0)*cos(theta0) - psi0*v0*sin(phi0)*sin(theta0)*cos(psi0) + psi0*v0*sin(psi0)*cos(phi0) - psi0*w0*sin(phi0)*sin(psi0) - psi0*w0*sin(theta0)*cos(phi0)*cos(psi0) + theta0*u0*sin(psi0)*sin(theta0) - theta0*v0*sin(phi0)*sin(psi0)*cos(theta0) - theta0*w0*sin(psi0)*cos(phi0)*cos(theta0);
-        -phi0*v0*cos(phi0)*cos(theta0) + phi0*w0*sin(phi0)*cos(theta0) + theta0*u0*cos(theta0) + theta0*v0*sin(phi0)*sin(theta0) + theta0*w0*sin(theta0)*cos(phi0);
-        -g*psi0*sin(psi0)*cos(theta0) - g*theta0*sin(theta0)*cos(psi0) - g*cos(psi0)*cos(theta0) + q0*w0 - r0*v0 + T0*xi0*sin(xi0)*cos(zeta0)/M + T0*zeta0*sin(zeta0)*cos(xi0)/M;
-        g*phi0*sin(phi0)*sin(psi0) + g*phi0*sin(theta0)*cos(phi0)*cos(psi0) - g*psi0*sin(phi0)*sin(psi0)*sin(theta0) - g*psi0*cos(phi0)*cos(psi0) + g*theta0*sin(phi0)*cos(psi0)*cos(theta0) - g*sin(phi0)*sin(theta0)*cos(psi0) + g*sin(psi0)*cos(phi0) - p0*w0 + r0*u0 - T0*zeta0*cos(zeta0)/M;
-        -g*phi0*sin(phi0)*sin(theta0)*cos(psi0) + g*phi0*sin(psi0)*cos(phi0) + g*psi0*sin(phi0)*cos(psi0) - g*psi0*sin(psi0)*sin(theta0)*cos(phi0) + g*theta0*cos(phi0)*cos(psi0)*cos(theta0) - g*sin(phi0)*sin(psi0) - g*sin(theta0)*cos(phi0)*cos(psi0) + p0*v0 - q0*u0 + T0*xi0*cos(xi0)*cos(zeta0)/M - T0*zeta0*sin(xi0)*sin(zeta0)/M;
-        -phi0*q0*cos(phi0)*tan(theta0) + phi0*r0*sin(phi0)*tan(theta0) - q0*theta0*sin(phi0)*tan(theta0)^2 - q0*theta0*sin(phi0) - r0*theta0*cos(phi0)*tan(theta0)^2 - r0*theta0*cos(phi0);
+        -phi0*v0*sin(phi0)*sin(psi0) - phi0*v0*sin(theta0)*cos(phi0)*cos(psi0) + phi0*w0*sin(phi0)*sin(theta0)*cos(psi0) - phi0*w0*sin(psi0)*cos(phi0) + psi0*u0*sin(psi0)*cos(theta0) + psi0*v0*sin(phi0)*sin(psi0)*sin(theta0) + psi0*v0*cos(phi0)*cos(psi0) - psi0*w0*sin(phi0)*cos(psi0) + psi0*w0*sin(psi0)*sin(theta0)*cos(phi0) + q0*z0 - r0*y0 + theta0*u0*sin(theta0)*cos(psi0) - theta0*v0*sin(phi0)*cos(psi0)*cos(theta0) - theta0*w0*cos(phi0)*cos(psi0)*cos(theta0);
+        -p0*z0 + phi0*v0*sin(phi0)*cos(psi0) - phi0*v0*sin(psi0)*sin(theta0)*cos(phi0) + phi0*w0*sin(phi0)*sin(psi0)*sin(theta0) + phi0*w0*cos(phi0)*cos(psi0) - psi0*u0*cos(psi0)*cos(theta0) - psi0*v0*sin(phi0)*sin(theta0)*cos(psi0) + psi0*v0*sin(psi0)*cos(phi0) - psi0*w0*sin(phi0)*sin(psi0) - psi0*w0*sin(theta0)*cos(phi0)*cos(psi0) + r0*x0 + theta0*u0*sin(psi0)*sin(theta0) - theta0*v0*sin(phi0)*sin(psi0)*cos(theta0) - theta0*w0*sin(psi0)*cos(phi0)*cos(theta0);
+        p0*y0 - phi0*v0*cos(phi0)*cos(theta0) + phi0*w0*sin(phi0)*cos(theta0) - q0*x0 + theta0*u0*cos(theta0) + theta0*v0*sin(phi0)*sin(theta0) + theta0*w0*sin(theta0)*cos(phi0);
+        -g*psi0*sin(psi0)*cos(theta0) - g*theta0*sin(theta0)*cos(psi0) - g*cos(psi0)*cos(theta0) + 2*p0*q0*y0 + 2*p0*r0*z0 - 2*q0^2*x0 + q0*w0 - 2*r0^2*x0 - r0*v0 + T0*xi0*sin(xi0)*cos(zeta0)/M + T0*zeta0*sin(zeta0)*cos(xi0)/M;
+        g*phi0*sin(phi0)*sin(psi0) + g*phi0*sin(theta0)*cos(phi0)*cos(psi0) - g*psi0*sin(phi0)*sin(psi0)*sin(theta0) - g*psi0*cos(phi0)*cos(psi0) + g*theta0*sin(phi0)*cos(psi0)*cos(theta0) - g*sin(phi0)*sin(theta0)*cos(psi0) + g*sin(psi0)*cos(phi0) - 2*p0^2*y0 + 2*p0*q0*x0 - p0*w0 + 2*q0*r0*z0 - 2*r0^2*y0 + r0*u0 - T0*zeta0*cos(zeta0)/M;
+        -g*phi0*sin(phi0)*sin(theta0)*cos(psi0) + g*phi0*sin(psi0)*cos(phi0) + g*psi0*sin(phi0)*cos(psi0) - g*psi0*sin(psi0)*sin(theta0)*cos(phi0) + g*theta0*cos(phi0)*cos(psi0)*cos(theta0) - g*sin(phi0)*sin(psi0) - g*sin(theta0)*cos(phi0)*cos(psi0) - 2*p0^2*z0 + 2*p0*r0*x0 + p0*v0 - 2*q0^2*z0 + 2*q0*r0*y0 - q0*u0 + T0*xi0*cos(xi0)*cos(zeta0)/M - T0*zeta0*sin(xi0)*sin(zeta0)/M;
+        -phi0*q0*sin(theta0)*cos(phi0)/cos(theta0) + phi0*r0*sin(phi0)*sin(theta0)/cos(theta0) - q0*theta0*sin(phi0)/cos(theta0)^2 - r0*theta0*cos(phi0)/cos(theta0)^2;
         phi0*q0*sin(phi0) + phi0*r0*cos(phi0);
         -phi0*q0*cos(phi0)/cos(theta0) + phi0*r0*sin(phi0)/cos(theta0) - q0*theta0*sin(phi0)*sin(theta0)/cos(theta0)^2 - r0*theta0*sin(theta0)*cos(phi0)/cos(theta0)^2;
         -Iyy*q0*r0/Ixx + Izz*q0*r0/Ixx;
